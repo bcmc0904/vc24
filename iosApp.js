1e89960289ca408be33a8ba15f9b05ce54655026
drupalplus <bcmc0904@gmail.com>
2017-12-30 23:02:29 +0700

    IOS notification work

--- iosApp.js
+++ iosApp.js
@@ -1,7 +1,7 @@
 /**
  * React native app create for vc 24. Create by Thai
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   TouchableOpacity,
   View,
@@ -11,9 +11,15 @@ import {
   Alert,
   AsyncStorage,
   Text,
+  AppState
 } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
-import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from "react-native-fcm";
+import FCM, {
+  FCMEvent,
+  RemoteNotificationResult,
+  WillPresentNotificationResult,
+  NotificationType
+} from "react-native-fcm";
 
 const url = 'http://vanchuyen24.com/';
 // const url = 'http://drupalplus.org/';
@@ -27,9 +33,7 @@ const urlPost = 'http://fcm.drupalplus.org/fcm/apptoken';
 const defaultTitle = 'Vận chuyển 24';
 const authorization = 'vanchuyen24.com';
 const deviceId = DeviceInfo.getUniqueID();
-// const deviceId = "123";
-// const deviceModel = DeviceInfo.getModel();
-const deviceModel = "ipone7";
+const deviceModel = DeviceInfo.getModel();
 const WEBVIEW_REF = 'webview';
 
 export default class App extends Component {
@@ -44,73 +48,94 @@ export default class App extends Component {
     }
   }
 
-  componentDidMount(){
-    // console.log(DeviceInfo);
-    // console.log(deviceId);
-    // console.log(DeviceInfo.getUniqueID());
+  componentDidMount() {
+    AppState.addEventListener('change', this._handleAppStateChange);
+    FCM.setBadgeNumber(0);
     FCM.getInitialNotification().then(notif => {
       this.setState({
         initNotif: notif
-      })
+      });
     });
 
-    try{
-      let result = FCM.requestPermissions({badge: false, sound: true, alert: true});
-    } catch(e){
-      console.error(e);
+    try {
+      let result = FCM.requestPermissions({
+        badge: true,
+        sound: true,
+        alert: true
+      });
+    }
+    catch (e) {
+      // console.error(e);
       Alert.alert(
           defaultTitle,
           'Bạn phải bật thông báo cho Vận Chuyển 24 để có thể nhận các thông báo đơn hàng!',
           [
-            {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+              style: 'cancel'
+            },
             {text: 'OK', onPress: () => console.log('OK Pressed')},
           ],
-          { cancelable: false }
+          {cancelable: false}
       )
     }
 
     FCM.getFCMToken().then(token => {
+      // console.log("line 82 (getFCMToken)", token);
       this.setState({
         token: token
       });
-      // this.onChangeToken(token);
+      this.onChangeToken(token);
     });
 
-    if(Platform.OS === 'ios'){
+    /*if (Platform.OS === 'ios') {
       FCM.getAPNSToken().then(token => {
+        // console.log("line 91 (getAPNSToken)", token);
         this.setState({
           token: token
         });
         this.onChangeToken(token);
       });
-    }
+    }*/
 
     this.notificationListener = FCM.on(FCMEvent.Notification, notif => {
       console.log("Notification", notif);
-      if(notif.local_notification){
+      if (notif.local_notification) {
         return;
       }
-      if(notif.opened_from_tray){
+      if (notif.opened_from_tray) {
+        FCM.setBadgeNumber(0);
         return;
       }
 
-      if(Platform.OS ==='ios'){
+      /*if (Platform.OS === 'ios') {
         //optional
-        //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
-        //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
-        //notif._notificationType is available for iOS platfrom
-        switch(notif._notificationType){
+        //iOS requires developers to call completionHandler to end notification
+        // process. If you do not call it your background remote notifications
+        // could be throttled, to read more about it see the above
+        // documentation link. This library handles it for you automatically
+        // with default behavior (for remote notification, finish with NoData;
+        // for WillPresent, finish depend on "show_in_foreground"). However if
+        // you want to return different result, follow the following code to
+        // override notif._notificationType is available for iOS platfrom
+        switch (notif._notificationType) {
           case NotificationType.Remote:
-            notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
+            notif.finish(RemoteNotificationResult.NewData) //other types
+                                                           // available:
+                                                           // RemoteNotificationResult.NewData,
+                                                           // RemoteNotificationResult.ResultFailed
             break;
           case NotificationType.NotificationResponse:
             notif.finish();
             break;
           case NotificationType.WillPresent:
-            notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
+            notif.finish(WillPresentNotificationResult.All) //other types
+                                                            // available:
+                                                            // WillPresentNotificationResult.None
             break;
         }
-      }
+      }*/
 
       this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
         console.log("TOKEN (refreshUnsubscribe)", token);
@@ -118,19 +143,20 @@ export default class App extends Component {
       });
 
       // direct channel related methods are ios only
-      // directly channel is truned off in iOS by default, this method enables it
-      FCM.enableDirectChannel();
+      // directly channel is truned off in iOS by default, this method enables
+      // it
+      /*FCM.enableDirectChannel();
       this.channelConnectionListener = FCM.on(FCMEvent.DirectChannelConnectionChanged, (data) => {
         console.log('direct channel connected' + data);
       });
-      setTimeout(function() {
+      setTimeout(function () {
         FCM.isDirectChannelEstablished().then(d => console.log(d));
-      }, 1000);
+      }, 1000);*/
 
       //Push msg to screen
-      FCM.scheduleLocalNotification({
+      /*FCM.scheduleLocalNotification({
         id: notif.from || 'testnotif',
-        fire_date: new Date().getTime()+3000,
+        fire_date: new Date().getTime() + 3000,
         vibrate: 500,
         title: notif.fcm.title || "Vận chuyển 24",
         body: notif.fcm.body,
@@ -138,66 +164,78 @@ export default class App extends Component {
         priority: "high",
         show_in_foreground: true,
         icon: notif.fcm.icon || null,
-      });
+      });*/
     })
   }
 
   componentWillUnmount() {
     this.notificationListener.remove();
     this.refreshTokenListener.remove();
+    AppState.removeEventListener('change', this._handleAppStateChange);
   }
 
-  onChangeToken(token) { console.log("url change")
+  _handleAppStateChange = (nextAppState) => {
+    if (AppState.currentState = 'active')
+      FCM.setBadgeNumber(0);
+  }
+
+  onChangeToken(token) {
     //Get stored rid
     AsyncStorage.getItem('receive', (err, result) => {
       if (result) {
         let receive_data = JSON.parse(result);
         this.updateReceive(token, receive_data);
-      } else {
+      }
+      else {
         let receive_data = {
           rid: 0,
           uid: 0,
           token: this.state.token
         }
         this.updateReceive(token, receive_data);
-        /*AsyncStorage.setItem('receive', JSON.stringify(receive_data), () => {
-          AsyncStorage.getItem('receive', (err, result) => {
-            let receive_data = JSON.parse(result);
-
-          });
-        });*/
       }
     });
   }
 
   updateReceive(token, receive_data) {
     fetch(urlGetUid)
-        .then((response) => { console.log(response); console.log(receive_data);
-          if (response.status == 200) {
-            try {
-              let res_data = JSON.parse(response._bodyInit);
-              if (res_data) {
-                if (res_data.uid) { console.log(res_data.uid);
-                  if (res_data.uid != receive_data.uid) {
-                    if (receive_data.uid != 0) this.getReiceId(token, receive_data.uid, true);
-                    this.getReiceId(token, res_data.uid);
+      .then((response) => {
+        console.log(response);
+        console.log(receive_data);
+        if (response.status == 200) {
+          try {
+            let res_data = JSON.parse(response._bodyInit);
+            if (res_data) {
+              if (res_data.uid) {
+                console.log(res_data.uid);
+                if (res_data.uid != receive_data.uid) {
+                  if (receive_data.uid != 0) {
+                    this.getReiceId(token, receive_data.uid, true);
                   }
-                  else if (receive_data.token != token) {
-                    this.getReiceId(token, res_data.uid);
-                  }
-                } else {
-                  if (receive_data.uid != 0) this.getReiceId(token, receive_data.uid, true);
-                  else AsyncStorage.removeItem('receive');
+                  this.getReiceId(token, res_data.uid);
+                }
+                else if (receive_data.token != token) {
+                  this.getReiceId(token, res_data.uid);
                 }
               }
-            } catch (err) {
-              console.log(err);
+              else {
+                if (receive_data.uid != 0) {
+                  this.getReiceId(token, receive_data.uid, true);
+                }
+                else {
+                  AsyncStorage.removeItem('receive');
+                }
+              }
             }
           }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+          catch (err) {
+            console.log(err);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   getReiceId(token, uid, remove = false) {
@@ -215,33 +253,36 @@ export default class App extends Component {
         remove: remove
       })
     })
-        .then((response) => { console.log(response);
-          if (response.status == 200) {
-            try {
-              let res_data = JSON.parse(response._bodyInit);
-              if (res_data.status == 1) {
-                if (remove) {
-                  AsyncStorage.removeItem('receive');
-                } else {
-                  let receive_data = {
-                    rid: res_data.rid,
-                    uid: uid,
-                    token: token
-                  }
-                  AsyncStorage.setItem('receive', JSON.stringify(receive_data));
-                }
+    .then((response) => {
+      console.log(response);
+      if (response.status == 200) {
+        try {
+          let res_data = JSON.parse(response._bodyInit);
+          if (res_data.status == 1) {
+            if (remove) {
+              AsyncStorage.removeItem('receive');
+            }
+            else {
+              let receive_data = {
+                rid: res_data.rid,
+                uid: uid,
+                token: token
               }
-            } catch (err) {
-              console.log(err);
+              AsyncStorage.setItem('receive', JSON.stringify(receive_data));
             }
           }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+        }
+        catch (err) {
+          console.log(err);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 
-  _onNavigationStateChange(webViewState){ console.log(webViewState.url);
+  _onNavigationStateChange(webViewState) {
     if (webViewState.url == uriLogedIn1 || webViewState.url == uriLogedIn2 || webViewState.url == uriLogedIn3 || webViewState.url == uriLogedIn4 || webViewState.url == url) {
       this.onChangeToken(this.state.token);
     }
@@ -251,7 +292,7 @@ export default class App extends Component {
     return (
         <View style={styles.error}>
           <TouchableOpacity style={styles.button}>
-            <Text style={{color:'white'}} onPress={this.reload}>Reload</Text>
+            <Text style={{color: 'white'}} onPress={this.reload}>Reload</Text>
           </TouchableOpacity>
         </View>
     );
@@ -284,7 +325,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#eee',
+    paddingTop: 19
   },
   error: {
     flex: 1,
